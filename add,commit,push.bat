06b5f09d971e5d1ccaa41de9dd905ee716b9bebf
Sahil Rajput <sahilrajput03@gmail.com>
2020-03-06 12:33:41 +0530

    commit-via-script

--- add,commit,push.bat
+++ add,commit,push.bat
@@ -1,1 +1,1 @@
-git add . && git commit -m "commit-via-script" && git push && echo ....... && echo.Pushed to repo: chetanmishra8660.github.io && @pause && tree
+git add . && git commit -m "commit-via-script" && git push && echo ....... && echo.Pushed to repo: chetanmishra8660.github.io && @pause
